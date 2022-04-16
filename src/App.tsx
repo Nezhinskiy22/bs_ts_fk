a37cc34b2713d5a76fb42f49fd1f9d45c1f965b9
Nezhinskiy22 <nezhinskiy22@gmail.com>
2022-04-16 12:23:20 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import "./App.css";
+import CreateNotes from "./components/CreateNotes";
 import Header from "./components/Header";
 import NotesList from "./components/NotesList";
 import { Note } from "./models/note.model";
@@ -22,7 +23,12 @@ function App() {
       <Container className="mt-5">
         <Row>
           <Col>
-            <NotesList notes={notes} />
+            <NotesList notes={notes} setNotes={setNotes} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <CreateNotes notes={notes} setNotes={setNotes} />
           </Col>
         </Row>
       </Container>
--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import * as React from "react";
+import { Button, Card } from "react-bootstrap";
+import { Note } from "../models/note.model";
 
-function Notes() {
-  return <div>Notes</div>;
+interface INotesProps {
+  note: Note;
+  handleDelete: (id: string) => void;
 }
+
+const Notes: React.FC<INotesProps> = ({ note, handleDelete }) => {
+  return (
+    <div className="mb-3">
+      <Card style={{ background: note.color }}>
+        <Card.Body>
+          <Card.Title>{note.title}</Card.Title>
+          <Card.Text>{note.text}</Card.Text>
+          <Card.Subtitle className="text-muted">{note.date}</Card.Subtitle>
+          <Button
+            className="mt-3"
+            variant="danger"
+            onClick={() => handleDelete(note.id)}
+          >
+            Delete
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
 
 export default Notes;
